c5489709a08c3c138420b865d0cd188f2c9b4a52
manoti3 <hillarymanoti0@gmail.com>
2024-01-09 08:43:02 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const init = () => {
+    //The DOM to be fully loaded before adding event listeners
+  document.addEventListener('DOMContentLoaded', () => {
+    const inputForm = document.querySelector('form');
+    const titleElement = document.querySelector('#movieDetails h4');
+    const summaryElement = document
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
